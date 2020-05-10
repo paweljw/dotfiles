f8da4b4032ef0aa228262588d0ae6018de944f8a
Paweł J. Wal <pawel.wal@codesthq.com>
2020-05-10 15:06:20 +0200

    Changed dotfiles @ nie, 10 maj 2020, 15:06:20 CEST

--- aliases/bootstrap.zsh
+++ aliases/bootstrap.zsh
@@ -1,7 +1,6 @@
 source ~/.dotfiles/aliases/secrets.zsh
 
 source ~/.dotfiles/aliases/postgres.zsh
-source ~/.dotfiles/aliases/ohmyzsh.zsh
 source ~/.dotfiles/aliases/bundler.zsh
 source ~/.dotfiles/aliases/ruby.zsh
 source ~/.dotfiles/aliases/rails.zsh
@@ -12,11 +11,9 @@ source ~/.dotfiles/aliases/npm.zsh
 source ~/.dotfiles/aliases/cargo.zsh
 source ~/.dotfiles/aliases/foreman.zsh
 source ~/.dotfiles/aliases/aliases.zsh
-source ~/.dotfiles/aliases/spotify.zsh
 source ~/.dotfiles/aliases/elixir.zsh
 source ~/.dotfiles/aliases/harvest.zsh
 source ~/.dotfiles/aliases/misc.zsh
-source ~/.dotfiles/aliases/sublime.zsh
 source ~/.dotfiles/aliases/yarn.zsh
 source ~/.dotfiles/aliases/brew.zsh
 source ~/.dotfiles/aliases/nvim.zsh
